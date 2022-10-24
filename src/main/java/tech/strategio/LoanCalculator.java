3a14fa2170ee10a2e9430b720fe4a02bcd0a08cb
lks17 <lks17@students.uwf.edu>
2022-10-24 12:20:42 -0500

    Added getRemainingAmountIn3Months function to program to get the remaining amount of loan owed

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
      * 
-     * @param ???
-     * @return ???
+     * This method takes in the total amount of the loan and 
+     * calculates how much is needed to pay back in 3 months time
+     * if you only pay 10% per month of the loan.
+     * 
+     * @param amount : total loan amount
+     * @return remainingAmount : remaining amount to pay back in 3 months
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Add your code below
+        int tenPercent = 0;
+        int remainingAmount = amount;
+        
+        for(int i = 0; i < 3; i++) {
+        	 tenPercent = (int)(remainingAmount * 0.1);
+             remainingAmount = remainingAmount - tenPercent;
+             System.out.println(remainingAmount);
+        }
+        
+    	return remainingAmount;
     }
 
     public static void main(String[] args) {
+    	System.out.print("Please Enter an amount: ");
         Scanner scanner = new Scanner(System.in);
         int amount = scanner.nextInt();
         System.out.println(getRemainingAmountIn3Months(amount));
